4bae82c0269a2e0e45ce471f212e6eb29e23fc9c
EvgNik88 <evni88@gmail.com>
2023-11-08 15:08:07 +0300

    Created app for managing todo_lists

---
+++ todo_app.py
@@ -0,0 +1,85 @@
+from todo_list import ToDoList
+
+
+class TodoApp:
+    def __init__(self):
+        self.todo_lists = []
+
+    def create_list(self, name):
+        """Метод для создания нового списка задач"""
+        new_todo_list = ToDoList(name)
+        self.todo_lists.append(new_todo_list)
+        print(f'Создан новый список задач "{name}"')
+        print()
+
+    def delete_list(self, index):
+        """Метод для удаления списка задач"""
+        if index < 0 or index >= len(self.todo_lists):
+            print('Список задач не найден')
+            print()
+            return False
+
+        del self.todo_lists[index]
+        print('Список задач удален')
+        print()
+        return True
+
+    def edit_list(self, index):
+        """Метод для редактирования списка задач"""
+        if index < 0 or index >= len(self.todo_lists):
+            print('Список задач не найден')
+            print()
+            return False
+
+        todo_list_for_edit = self.todo_lists[index]
+        todo_list_for_edit.run()
+
+    def show_lists(self):
+        """Метод для вывода списка всех списков задач"""
+        if len(self.todo_lists) == 0:
+            print('Нет текущих списков задач')
+            return
+
+        print('Список всех списков задач:')
+        index = 1
+        for todo_list in self.todo_lists:
+            print(f'{index}. {todo_list.name}')
+            index += 1
+
+    def main(self):
+        """Метод для запуска программы"""
+        while True:
+            print('Меню:')
+            print('1. Создать новый список задач.')
+            print('2. Удалить список задач.')
+            print('3. Редактировать список задач.')
+            print('4. Показать все списки задач.')
+            print('5. Выйти.')
+            print()
+
+            choice = input('Выберите пункт меню: ')
+            print()
+
+            if choice == '1':
+                name = input('Введите название списка задач: ')
+                self.create_list(name)
+
+            elif choice == '2':
+                index = int(input('Введите номер списка задач для удаления: '))
+                self.delete_list(index - 1)
+
+            elif choice == '3':
+                index = int(input('Введите номер списка задач для редактирования: '))
+                self.edit_list(index - 1)
+
+            elif choice == '4':
+                self.show_lists()
+
+            elif choice == '5':
+                print('Программа завершена.')
+                break
+
+
+if __name__ == '__main__':
+    app = TodoApp()
+    app.main()
